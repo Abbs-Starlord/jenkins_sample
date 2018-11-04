f19f2baa54fd9c194e3aed1ec49f2396fb95d71c
Umer Abbasi <rajaumerabbasi@gmail.com>
2018-11-05 00:00:03 +1100

    v2

--- jenkins_sample/src/test/java/demo/sample/selen.java
+++ jenkins_sample/src/test/java/demo/sample/selen.java
@@ -12,40 +12,19 @@ public class selen {
 	@Test
 	public void test1() throws InterruptedException{
 		
-		//System.setProperty("webdriver.gecko.driver","/User/umerabbs/Downloads/geckodriver");
 		
-		//System.setProperty("webdriver.gecko.driver","/Users/umerabbs/Documents/workspace/testJava/geckodriver");
 		System.setProperty("webdriver.gecko.driver","./geckodriver");
 		
-		
-		//String path = System.getProperty("user.dir") + "/Users/umerabbs/Documents/workspace/testJava/geckodriver";
-		//System.setProperty("webdriver.gecko.driver", path);
-		
 		WebDriver driver = new FirefoxDriver();
-		//WebDriver driver = new ChromeDriver();
+		
+	    System.out.println("Opening Google");
 
-		
-		//driver.get("http://seleniumhq.org/");
-		//String appUrl = "https://accounts.google.com";
-		//driver.manage().window().maximize();
-		
 		String appUrl = "http://google.com";
 	    driver.get(appUrl);
 	    
-	    //driver.findElement(By.name("q")),sendKeys("download selenium");
-	    //driver.findElement(By.name("q")).sendKeys("download selenium");
-	    //Thread.sleep(100);
-//	    String title = driver.getTitle();
-//	    title.contains("Facebook")
-	    //WebElement searchTxtBox = driver.findElement(By.name("q"));
-	    //libUtil.waitVisible(driver, searchTxtBox, 10).sendKeys("Download Selenium");
+	    
 	    libUtil.waitVisible(driver, driver.findElement(By.name("q")), 10).sendKeys("Download Selenium");
 	    
-	    //driver.findElement(By.name("btnK")).click();
-	    //Thread.sleep(2000);
-	    
-	    //WebElement searchButton = driver.findElement(By.name("btnK"));
-	    //libUtil.waitClick(driver, searchButton, 10).click();
 	    libUtil.waitClick(driver, driver.findElement(By.name("btnK")), 10).click();
 
 
